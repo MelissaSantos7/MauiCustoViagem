f8acd5276077f33e130712e4865800745551870a
Usuario <Usuario@CPD03-M12>
2024-04-15 12:12:49 -0300

    Adicionar arquivos de projeto.

---
+++ MauiCustoViagem/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiCustoViagem
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PedagiosBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculoBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
