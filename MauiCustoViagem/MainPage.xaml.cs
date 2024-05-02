9e71ea6fc81d229fc268c6360b2626c31d93a71d
Usuario <Usuario@CPD02-M06>
2024-05-02 11:39:03 -0300

    .

--- MauiCustoViagem/MainPage.xaml.cs
+++ MauiCustoViagem/MainPage.xaml.cs
@@ -18,7 +18,39 @@
 
         private void CalculoBtn_Clicked(object sender, EventArgs e)
         {
+            //decimal valor_total_pedagios = PropriedadesApp.ListaPedagios.Sum(item => item.Valor);
+
+            if (string.IsNullOrEmpty(txt_distancia.Text))
+                throw new Exception("Por favor, preencha a distância.");
+
+            if (string.IsNullOrEmpty(txt_combustivel.Text))
+                throw new Exception("Por favoe, preencha a consumo do veiculo.");
+
+            if (string.IsNullOrEmpty(txt_combustivel.Text))
+                throw new Exception("Por favor , preencha o valor do combustível.");
+
+            decimal consumo = Convert.ToDecimal(txt_combustivel.Text);
+            decimal preco_combustivel = Convert.ToDecimal(txt_combustivel.Text);
+            decimal distancia = Convert.ToDecimal(txt_distancia.Text);
+
+            decimal consumo_carro = (distancia / consumo) * preco_combustivel;
+
+            //decimal custo_total = consumo_carro + valor_total_pedagios;
+
+            string origem = txt_origem.Text;
+            string destino = txt_destino.Text;
+
             //DisplayAlert("")
+        }
+
+        private void LimparBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LimparpedBtn_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 
